52258352844a317545507566ff988cc74596eecf
pradipk1 <pradipshukla12699@gmail.com>
2023-10-23 19:40:56 +0530

    logger removed from redux

--- ecommerce-1/src/Components/Redux/ReduxStore.js
+++ ecommerce-1/src/Components/Redux/ReduxStore.js
@@ -1,13 +1,13 @@
 import {legacy_createStore, combineReducers, applyMiddleware} from 'redux';
 import productsReducer from './Reducers/productsReducer';
 import cartReducer from './Reducers/cartReducer';
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 
 const combinedReducer = combineReducers({
     products: productsReducer,
     cart: cartReducer
 });
 
-const reduxStore = legacy_createStore(combinedReducer, applyMiddleware(logger));
+const reduxStore = legacy_createStore(combinedReducer, applyMiddleware());
 
 export default reduxStore;
